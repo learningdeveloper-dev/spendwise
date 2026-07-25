b8bfa3a478182d2c5828c0f268d724f82a0151ce
learningdeveloper-dev <learningdeveloper-dev@users.noreply.github.com>
2026-07-25 16:01:33 +0530

    Added audit fields to DTOs

---
+++ src/main/java/com/learningdeveloper/spendwise/base/dto/BaseDto.java
@@ -0,0 +1,8 @@
+package com.learningdeveloper.spendwise.base.dto;
+
+public class BaseDto {
+    private String createdBy;
+    private String modifiedBy;
+    private String createdOn;
+    private String modifiedOn;
+}
--- src/main/java/com/learningdeveloper/spendwise/users/dto/User.java
+++ src/main/java/com/learningdeveloper/spendwise/users/dto/User.java
@@ -1,5 +1,6 @@
 package com.learningdeveloper.spendwise.users.dto;
 
+import com.learningdeveloper.spendwise.base.dto.BaseDto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +8,7 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
-public class User {
+public class User extends BaseDto {
     private Long userId;
     private String displayName;
     private String emailId;
